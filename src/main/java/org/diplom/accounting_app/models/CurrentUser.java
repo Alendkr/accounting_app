ccea09ab2f8f18b69f755ab2a37f763c1c048236
Алексей Кренделев <alendk.real@gmail.com>
2025-02-11 19:19:40 +0300

    Tak nado

---
+++ src/main/java/org/diplom/accounting_app/models/CurrentUser.java
@@ -0,0 +1,14 @@
+package org.diplom.accounting_app.models;
+
+public class CurrentUser {
+
+    private static User currentUser;
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+}
